d4e5073acdaa5a39f514b6a2910b77d0bdc89a7a
Dmitry Smirnov <saucechilidev@gmail.com>
2025-05-07 20:05:51 +0300

    feat: added entry-point plugin class

---
+++ src/main/java/vlbl/stats/plugin/StatsPlugin.java
@@ -0,0 +1,30 @@
+package vlbl.stats.plugin;
+
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.node.DiscoveryNodes;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.IndexScopedSettings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.settings.SettingsFilter;
+import org.opensearch.plugins.ActionPlugin;
+import org.opensearch.plugins.Plugin;
+import org.opensearch.rest.RestController;
+import org.opensearch.rest.RestHandler;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class StatsPlugin extends Plugin implements ActionPlugin {
+    @Override
+    public List<RestHandler> getRestHandlers(
+            Settings settings,
+            RestController restController,
+            ClusterSettings clusterSettings,
+            IndexScopedSettings indexScopedSettings,
+            SettingsFilter settingsFilter,
+            IndexNameExpressionResolver indexNameExpressionResolver,
+            Supplier<DiscoveryNodes> nodesInCluster
+    ) {
+        return List.of();
+    }
+}
